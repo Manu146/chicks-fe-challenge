1e5cab84ebb17bde9480d59c65e92f2a83a5f2ac
Manu <manuel_8ale@hotmail.com>
2024-07-25 17:42:14 -0400

    create items page header

---
+++ src/features/Items/components/ItemsHeader.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const ItemsHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+  margin: 2rem 0;
+`;
+
+export default ItemsHeader;
