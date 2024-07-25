d4ddd652dd86e387eccda1d65629909f752237aa
Manu <manuel_8ale@hotmail.com>
2024-07-25 17:40:39 -0400

    style h1 in main container

--- src/components/layout/MainContainer.jsx
+++ src/components/layout/MainContainer.jsx
@@ -4,6 +4,12 @@ const MainContainer = styled.div`
   width: 80%;
   margin: 6rem auto 2rem;
   max-width: 1200px;
+  & h1 {
+    font-size: 3rem;
+    color: white;
+    font-weight: 700;
+    margin-bottom: 1.25rem;
+  }
 `;
 
 export default MainContainer;
