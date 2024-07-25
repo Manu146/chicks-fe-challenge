8e45afbb3848f67318f43470e72e0723c18a2def
Manu <manuel_8ale@hotmail.com>
2024-07-25 17:43:26 -0400

    change items data, add content

--- src/features/Items/index.jsx
+++ src/features/Items/index.jsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 import ItemsSection from "./components/ItemsSection";
 import ItemsGrid from "./components/ItemsGrid";
-import { useState } from "react";
+import { useState, useMemo } from "react";
+import SearchBar from "../../components/ui/SearchBar";
+import FiltersBar from "./components/FiltersBar";
+import FilterDropdown from "./components/FilterDropdown";
+import ItemsHeader from "./components/ItemsHeader";
+import ItemsCount from "./components/ItemsCount";
+import ItemsPagination from "./components/ItemsPagination";
 
 const Container = styled.div``;
 
@@ -14,6 +20,7 @@ const itemsData = [
     stock: 8,
     img: "/B._F._Sword_item_HD.png",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    type: "Unique",
   },
   {
     name: "Needlessly Large Rod",
@@ -23,6 +30,7 @@ const itemsData = [
     stock: 12,
     img: "/Needlessly_Large_Rod_item_HD.png",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    type: "Unique",
   },
   {
     name: "Giant's Belt",
@@ -31,6 +39,7 @@ const itemsData = [
     stock: 6,
     img: "/Giant's_Belt_item_HD.png",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    type: "Unique",
   },
   {
     name: "Sunfire Aegis",
@@ -39,6 +48,7 @@ const itemsData = [
     stock: 5,
     img: "/Sunfire_Aegis_item_HD.png",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    type: "Limited",
   },
   {
     name: "Dead Man's Plate",
@@ -47,15 +57,77 @@ const itemsData = [
     stock: 0,
     img: "/Dead_Man's_Plate_item_HD.png",
     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    type: "Legendary",
   },
 ];
 
+const sortByOpts = ["Featured", "Price: Low to high"];
+const typeOpts = ["All", "Legendary", "Limited", "Unique"];
+const gameOpts = ["All", "League of legends", "World of Warcraft"];
+
 export default function Items() {
+  const [sortBy, setSortBy] = useState(sortByOpts[0]);
+  const [search, setSearch] = useState("");
+  const [max, setMax] = useState(10000);
+  const [min, setMin] = useState(0);
+  const [type, setType] = useState(typeOpts[0]);
+  const [game, setGame] = useState(gameOpts[0]);
+
+  let filtered1 = useMemo(() => {
+    if (search === "") return itemsData;
+    return itemsData.filter(({ name }) => name.includes(search));
+  }, [search]);
+  let filtered2 = useMemo(
+    () =>
+      filtered1.filter(({ price, salePrice }) => {
+        let finalPrice = salePrice ? salePrice : price;
+        return finalPrice < max && finalPrice > min;
+      }),
+    [filtered1, max, min]
+  );
+  let filtered3 = useMemo(() => {
+    if (type === "All") return filtered2;
+    return filtered2.filter(({ type: itemType }) => itemType === type);
+  }, [filtered2, type]);
+  let filtered4 = useMemo(() => {
+    if (game === "All") return filtered3;
+    return filtered3.filter(
+      ({ game: itemGame }) => itemGame.toLowerCase() === game.toLowerCase()
+    );
+  }, [filtered3, game]);
+
   return (
     <Container>
-      <div>filters</div>
+      <FiltersBar>
+        <FilterDropdown
+          type="game"
+          opts={gameOpts}
+          value={game}
+          setStateFn={setGame}
+        />
+        <SearchBar searchValue={search} setSearchFn={setSearch} />
+        <div>
+          <FilterDropdown type="price" value={[min, max]} />
+          <FilterDropdown
+            type="type"
+            opts={typeOpts}
+            value={type}
+            setStateFn={setType}
+          />
+        </div>
+      </FiltersBar>
       <ItemsSection>
-        <ItemsGrid items={itemsData} />
+        <ItemsHeader>
+          <ItemsCount total={itemsData.length} count={filtered4.length} />
+          <FilterDropdown
+            type="sort"
+            opts={sortByOpts}
+            value={sortBy}
+            setStateFn={setSortBy}
+          />
+        </ItemsHeader>
+        <ItemsGrid items={filtered4} />
+        <ItemsPagination nOfPages={6} currentPage={1} />
       </ItemsSection>
     </Container>
   );
